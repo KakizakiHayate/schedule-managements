6e7d822fb172a6bb392df89838a3b7c8296d5b73
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-08 23:51:10 +0900

    Appファイルを移動

---
+++ ScheduleManagements/Applecation/ScheduleManagementsApp.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleManagementsApp.swift
+//  ScheduleManagements
+//
+//  Created by 柿崎逸 on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleManagementsApp: App {
+    // MARK: - Body
+    var body: some Scene {
+        WindowGroup {
+            OneWeekSelectView()
+        }
+    } // body
+} // App
