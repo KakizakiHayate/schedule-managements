9fe6fd135494d3075021839c36ac0646d1c81bb0
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-10 23:37:30 +0900

    Modelを増やした時にprorocolを使うため追加しておく

---
+++ ScheduleManagements/Protocol/OneWeek.swift
@@ -0,0 +1,18 @@
+//
+//  OneWeek.swift
+//  ScheduleManagements
+//
+//  Created by 柿崎逸 on 2023/10/10.
+//
+
+import Foundation
+
+protocol OneWeek {
+
+}
+
+extension OneWeek {
+    func raed() {
+        
+    }
+}
