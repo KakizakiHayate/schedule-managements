147288ea7f08afece264d8e45bd176d1703e5af5
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-10 23:37:56 +0900

    修正画面のViewModelも追加しておく

---
+++ ScheduleManagements/Screen/Revision/RevisionViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  RevisionViewModel.swift
+//  ScheduleManagements
+//
+//  Created by 柿崎逸 on 2023/10/10.
+//
+
+import Foundation
+
+class RevisionViewModel: ObservableObject {
+    // MARK: - Property Wrappers
+    
+}
+
+extension RevisionViewModel {
+    // MARK: - Methods
+}
