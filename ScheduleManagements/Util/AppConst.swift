b9257be8706c4172650d96e3a6eb108c1465718b
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-12 20:05:18 +0900

    AppConstの追加

---
+++ ScheduleManagements/Util/AppConst.swift
@@ -0,0 +1,20 @@
+//
+//  AppConst.swift
+//  ScheduleManagements
+//
+//  Created by 柿崎逸 on 2023/10/12.
+//
+
+import Foundation
+
+enum AppConst {
+    // MARK: - DefaultValue
+    enum DefaultValue {
+        static let schedule = ["", "", "", "", "", ""]
+        static let scheduleCount = 6
+    }
+    // MARK: - Empty
+    enum Empty {
+        static let emptyText = ""
+    }
+}
