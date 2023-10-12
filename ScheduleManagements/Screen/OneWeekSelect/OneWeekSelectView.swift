d2aaff3a362546bc95a1ff09970475fc17108303
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-12 20:48:03 +0900

    先程のコミット漏れ(選択画面)

--- ScheduleManagements/Screen/OneWeekSelect/OneWeekSelectView.swift
+++ ScheduleManagements/Screen/OneWeekSelect/OneWeekSelectView.swift
@@ -7,118 +7,109 @@
 
 import SwiftUI
 
+/// 曜日選択画面
 struct OneWeekSelectView: View {
+
+    // MARK: - Body
     var body: some View {
         NavigationStack {
             ZStack {
-                LinearGradient(gradient: Gradient(colors: [Color.customColorPurple, .white]),
+                LinearGradient(gradient: Gradient(colors: [Color.customColorPurple,
+                                                           .white]),
                                startPoint: .top,
                                endPoint: .bottom)
                 .ignoresSafeArea()
                 VStack {
                     HStack {
-                        
                         VStack {
-                            //                            NavigationLink {
-                            //                                SaturdayView(saturDayModel: SaturDayModel(), migration: Migration())
-                            //
-                            //                            } label: {
-                            //                                Text("土")
-                            //                                    .font(.title)
-                            //                                    .foregroundColor(.white)
-                            //                                    .frame(width: 65, height: 65)
-                            //                                    .background(Color.customColorPurple)
-                            //                                    .cornerRadius(40)
-                            //                            }.padding(.bottom)
-                            
-                            //                            NavigationLink {
-                            //                                SundayView(sunDayModel: SunDayModel(), migration: Migration())
-                            //
-                            //                            } label: {
-                            //                                Text("日")
-                            //                                    .font(.title)
-                            //                                    .foregroundColor(.white)
-                            //                                    .frame(width: 65, height: 65)
-                            //                                    .background(Color.customColorPurple)
-                            //                                    .cornerRadius(40)
-                            //                            }.padding(.top)
-                            //                        }.padding(.trailing)
-                            
-                            VStack {
-                                //                            NavigationLink {
-                                //                                WednesdayView(wednesDayModel: WednesDayModel(), migration: Migration())
-                                //
-                                //                            } label: {
-                                //                                Text("水")
-                                //                                    .font(.title)
-                                //                                    .foregroundColor(.white)
-                                //                                    .frame(width: 65, height: 65)
-                                //                                    .background(Color.customColorPurple)
-                                //                                    .cornerRadius(40)
-                                //                            }.padding(.bottom)
-                                
-                                //                            NavigationLink {
-                                //                                ThursdayView(thursDayModel: ThursDayModel(), migration: Migration())
-                                //
-                                //                            } label: {
-                                //                                Text("木")
-                                //                                    .font(.title)
-                                //                                    .foregroundColor(.white)
-                                //                                    .frame(width: 65, height: 65)
-                                //                                    .background(Color.customColorPurple)
-                                //                                    .cornerRadius(40)
-                                //                            }
-                                
-                                //                            NavigationLink {
-                                //                                FridayView(friDayModel: FriDayModel(), migration: Migration())
-                                //
-                                //                            } label: {
-                                //                                Text("金")
-                                //                                    .font(.title)
-                                //                                    .foregroundColor(.white)
-                                //                                    .frame(width: 65, height: 65)
-                                //                                    .background(Color.customColorPurple)
-                                //                                    .cornerRadius(40)
-                                //                            }.padding(.top)
-                                //                        }
-                                
-                                VStack {
-                                    NavigationLink {
-                                        OneWeekView<Monday>()
+                            NavigationLink {
+                                OneWeekView<Saturday>()
+                            } label: {
+                                Text("土")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .frame(width: 65, height: 65)
+                                    .background(Color.customColorPurple)
+                                    .cornerRadius(40)
+                            }.padding(.bottom)
 
-                                    } label: {
-                                        Text("月")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                            .frame(width: 65, height: 65)
-                                            .background(Color.customColorPurple)
-                                            .cornerRadius(40)
-                                    }.padding(.bottom)
-                                    
-                                    //                            NavigationLink {
-                                    //                                TuesdayView(tuesDayModel: TuesDayModel(), migration: Migration())
-                                    //
-                                    //                            } label: {
-                                    //                                Text("火")
-                                    //                                    .font(.title)
-                                    //                                    .foregroundColor(.white)
-                                    //                                    .frame(width: 65, height: 65)
-                                    //                                    .background(Color.customColorPurple)
-                                    //                                    .cornerRadius(40)
-                                    //                            }.padding(.top)
-                                    //                        }.padding(.leading)
-                                    //
-                                    //                    }
-                                }
+                            NavigationLink {
+                                OneWeekView<Sunday>()
+                            } label: {
+                                Text("日")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .frame(width: 65, height: 65)
+                                    .background(Color.customColorPurple)
+                                    .cornerRadius(40)
+                            }.padding(.top)
+                        }.padding(.trailing)
+
+                        VStack {
+                            NavigationLink {
+                                OneWeekView<Wednesday>()
+                            } label: {
+                                Text("水")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .frame(width: 65, height: 65)
+                                    .background(Color.customColorPurple)
+                                    .cornerRadius(40)
+                            }.padding(.bottom)
+                            NavigationLink {
+                                OneWeekView<Thursday>()
+                            } label: {
+                                Text("木")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .frame(width: 65, height: 65)
+                                    .background(Color.customColorPurple)
+                                    .cornerRadius(40)
                             }
+                            NavigationLink {
+                                OneWeekView<Friday>()
+                            } label: {
+                                Text("金")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .frame(width: 65, height: 65)
+                                    .background(Color.customColorPurple)
+                                    .cornerRadius(40)
+                            }.padding(.top)
                         }
+
+                        VStack {
+                            NavigationLink {
+                                OneWeekView<Monday>()
+
+                            } label: {
+                                Text("月")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .frame(width: 65, height: 65)
+                                    .background(Color.customColorPurple)
+                                    .cornerRadius(40)
+                            }.padding(.bottom)
+
+                            NavigationLink {
+                                OneWeekView<Tuesday>()
+                            } label: {
+                                Text("火")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .frame(width: 65, height: 65)
+                                    .background(Color.customColorPurple)
+                                    .cornerRadius(40)
+                            }.padding(.top)
+                        }.padding(.leading)
                     }
                 }
             }
         }
-    }
-}
+    } // body
+} // view
 
+// MARK: - Preview
 #Preview {
     OneWeekSelectView()
 }
