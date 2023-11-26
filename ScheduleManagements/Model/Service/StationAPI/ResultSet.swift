45e2aa228fb509fb50a3833a415ce4942083f67a
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-27 00:11:11 +0900

    StationAPIに使うModelファイルを追加

---
+++ ScheduleManagements/Model/Service/StationAPI/ResultSet.swift
@@ -0,0 +1,24 @@
+//
+//  ResultSet.swift
+//  ScheduleManagements
+//
+//  Created by 柿崎逸 on 2023/11/26.
+//
+
+import Foundation
+
+struct ResultSet: Decodable {
+    let apiVersion: String
+    let max: String
+    let offset: String
+    let engineVersion: String
+    let point: [Point]
+
+    enum CodingKeys: String, CodingKey {
+        case apiVersion
+        case max
+        case offset
+        case engineVersion
+        case point = "Point"
+    }
+}
